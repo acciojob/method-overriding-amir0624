04028ac17c741a57ea4da491a8f7ca59eeda8222
amir0624 <mohammadamir0624@gmail.com>
2023-01-13 01:03:08 +0530

    Accio Assignment by Mohammad Amir

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,3 +6,14 @@ public class Main {
          System.out.println(obj.meth());
     }
 }
+ class A{
+        public static String meth(){
+            return "Invoking method from class A";
+        }
+
+    }
+    class B extends A{
+        public static String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
