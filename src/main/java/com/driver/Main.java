7589e02fa86873ee6481108639a0be71e171abb3
amir0624 <mohammadamir0624@gmail.com>
2023-01-13 12:18:02 +0530

    Making sperate classes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,18 +2,8 @@ package com.driver;
 
 public class Main {
      public static void main(String[] args) {
-         B obj= new B();
+        A obj= new B();
          System.out.println(obj.meth());
     }
 }
- class A{
-        public static String meth(){
-            return "Invoking method from class A";
-        }
 
-    }
-    class B extends A{
-        public static String meth(){
-            return "Method is overridden in Extendend class B";
-        }
-    }
