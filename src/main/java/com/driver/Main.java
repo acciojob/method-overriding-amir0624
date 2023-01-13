dcffe4adc21ff663d18b82a236b50b339bfff55d
amir0624 <mohammadamir0624@gmail.com>
2023-01-13 14:25:40 +0530

    Merging into main class.

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -7,17 +7,15 @@ public class Main {
         A obj= new B();
         obj.meth();
     }
+
     public static class A{
         public String meth(){
             return "Invoking method from class A";
-
         }
     }
     public static class B extends A{
         public   String meth(){
             return "Method is overridden in Extendend class B";
-
         }
     }
-
 }
